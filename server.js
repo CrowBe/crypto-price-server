1d40c6648da6326901c294bdb6c3b95a304644a8
CrowBe <benny_crow91@hotmail.com>
2020-06-04 13:32:52 +0900

    allowing cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // CORS middleware
-app.use(cors({origin: "https://build-six-delta.now.sh/"}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Welcome')
